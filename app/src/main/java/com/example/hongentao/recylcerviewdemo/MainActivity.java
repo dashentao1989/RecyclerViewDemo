705408d12bd7ceee608e324c7064d109ced49976
hongentao <hongentao@youzan.com>
2016-06-20 16:25:42 +0800

    add haha for test

--- app/src/main/java/com/example/hongentao/recylcerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/hongentao/recylcerviewdemo/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // haha
         init();
         initView();
         initListener();
