cc203e87415bad8844dfb1e4905eece9fcc7f723
hongentao <hongentao@youzan.com>
2016-06-20 17:23:33 +0800

    modify some code

--- app/src/main/java/com/example/hongentao/recylcerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/hongentao/recylcerviewdemo/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // init
         init();
+        // init from v1.1
         initView();
         initListener();
         initAdapter();
